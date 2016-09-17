cc0876c45052ef752b15f61d3fd1654123a406aa
rastinprog <laedrainouve@emeil.ir>
2016-09-17 23:25:23 +0430

    Update bot.lua

--- system/bot/bot.lua
+++ system/bot/bot.lua
@@ -234,10 +234,10 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {155294499,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'system/data/adv.json'},
-    about_text = [[*IN THE NAME OF ALLAH*
+    about_text = [[*IN THE NAME OF god *
 This is an original bot and based on (AdvanSource).
 Copyright all right reserved and you must respect all laws.
 
